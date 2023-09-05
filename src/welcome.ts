d67e7b3ffc33e2656d9813002bbaa1892fb64491
eneyret <manu@holidee.fr>
2023-09-05 13:11:10 +0200

    Update welcome.ts

--- src/welcome.ts
+++ src/welcome.ts
@@ -17,7 +17,7 @@ WA.onInit().then(() => {
 	});
 
 	WA.room.area.onEnter('Jira').subscribe(() => {
-		WA.nav.openTab('https://holidee.atlassian.net/servicedesk/customer/portals');
+		WA.nav.openTab('https://holidee.atlassian.net/servicedesk');
 	});
 
 	WA.room.area.onEnter('HolideeWebsite').subscribe(() => {
